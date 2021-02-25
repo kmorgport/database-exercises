587a7736fe01e43a6d3c9fd99ab3aa22d7cc467d
Kole Morgan <kmorgan0057@gmail.com>
2021-02-25 16:56:53 -0600

    mysql clauses

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,5 +1,7 @@
 USE codeup_test_db;
 
+TRUNCATE albums;
+
 CREATE TABLE `albums` (
                           `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
                           `artist` varchar(100) NOT NULL,
--- select_exercises.sql
+++ select_exercises.sql
@@ -1,6 +1,6 @@
 USE codeup_test_db;
 
-SELECT name FROM albums WHERE artist = 'Pink Floyd';
+SELECT * FROM albums WHERE artist = 'Pink Floyd';
 SELECT release_date FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
 SELECT genre FROM albums WHERE name = 'Nevermind';
 SELECT * FROM albums WHERE release_date BETWEEN 1990 AND 1999;
