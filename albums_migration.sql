a5ac548781ceb250100c326389d3d1108239fc18
Kole Morgan <kmorgan0057@gmail.com>
2021-02-25 11:27:29 -0600

    SQL statement exercises

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,11 +1,11 @@
 USE codeup_test_db;
 
 CREATE TABLE `albums` (
-                          `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+                          `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
                           `artist` varchar(100) NOT NULL,
                           `name` varchar(100) NOT NULL,
-                          `release_date` int(4) NOT NULL,
-                          `sales` float NOT NULL,
+                          `release_date` int NOT NULL,
+                          `sales` decimal(6,3) NOT NULL,
                           `genre` varchar(100) NOT NULL,
                           PRIMARY KEY (id)
 );
