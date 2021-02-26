ea8610bb4fb8094d19a6c6a33b95424735697d8f
Kole Morgan <kmorgan0057@gmail.com>
2021-02-26 12:31:33 -0600

    SQL function exercises

---
+++ limit_exercises.sql
@@ -0,0 +1,18 @@
+USE employees;
+
+SELECT DISTINCT last_name FROM employees
+ORDER BY last_name DESC
+LIMIT 10;
+
+
+SELECT * FROM employees
+WHERE hire_date > '199%'
+AND birth_date LIKE '%12-25'
+ORDER BY birth_date, hire_date DESC
+LIMIT 5;
+
+SELECT * FROM employees
+WHERE hire_date > '199%'
+AND birth_date LIKE '%12-25'
+ORDER BY birth_date, hire_date DESC
+LIMIT 5 OFFSET  45;
