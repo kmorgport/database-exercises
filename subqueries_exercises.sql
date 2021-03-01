e510491f8e199505f00550f940de59c234c5e841
Kole Morgan <kmorgan0057@gmail.com>
2021-03-01 16:47:45 -0600

    sql sub queries

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -7,3 +7,11 @@ WHERE hire_date IN (
     FROM employees
     WHERE emp_no = 101010
     );
+
+SELECT title
+FROM titles
+WHERE emp_no IN (
+    SELECT emp_no
+    FROM employees
+    WHERE first_name = 'Aamod'
+    );
